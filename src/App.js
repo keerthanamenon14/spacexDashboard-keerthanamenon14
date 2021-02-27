5c9bde147b86e384258e07fc6a8ee5e8e357a5bd
keerthanamenon14 <78547677+keerthanamenon14@users.noreply.github.com>
2021-02-27 21:18:47 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
-import Dashboard from './_pages/Dashboard'
+import "./App.css";
+import { Provider } from "react-redux";
+import { store, persistor } from "./_redux/_store";
+import { PersistGate } from "redux-persist/integration/react";
+import Dashboard from "./_pages/Dashboard";
+import { history } from "./_redux/_store/history";
+import { Router } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App" id="spacexWrapper">
-        <Dashboard/>
-    </div>
+    <Provider store={store}>
+      <div className="App" id="spacexWrapper">
+        <Router history={history}>
+          <PersistGate persistor={persistor}>
+            <Dashboard />
+          </PersistGate>
+        </Router>
+      </div>
+    </Provider>
   );
 }
 
